49af3c4dfe42d5e29ec4128df7045a75f8758d31
mahi028 <tewarimohit028@gmail.com>
2025-09-21 20:25:46 +0530

    Some visual changes to icon and using ascii art on termial on startup

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
+from assets.ascii_art import banner
+print(banner)
+
 from waitress import serve
 from app import app
 
 if __name__ == "__main__":
+    
     print("Application running on all networks")
     print("Visit app on local address: http://127.0.0.1:9876/")
     print("Visit app on network: http://your-device-ip:9876/")
