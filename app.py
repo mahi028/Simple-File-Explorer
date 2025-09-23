566fdc407496ccedeb953733d42469a3e8d749b4
mahi028 <tewarimohit028@gmail.com>
2025-09-23 16:24:54 +0530

    WEBUI

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ api.init_app(app)
 def index():
     return render_template("index.html")
 
+@app.route("/health")
+def health():
+    return {"msg":"Backend Running"}
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=9876)
--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,28 @@ from assets.ascii_art import banner
 print(banner)
 
 from waitress import serve
+from webui import webui
 from app import app
+import threading
+
+LOACL_HOST = "127.0.0.1"
+OPEN_HOST = "0.0.0.0"
+BACKEND_PORT = 9876
+WEBUI_PORT = 9877
+
+web_window = webui.Window()
 
 if __name__ == "__main__":
-    
+    # Print some info
     print("Application running on all networks")
-    print("Visit app on local address: http://127.0.0.1:9876/")
-    print("Visit app on network: http://your-device-ip:9876/")
-    serve(app, host="0.0.0.0", port=9876)
+    print(f"Visit app on local address: http://{LOACL_HOST}:{BACKEND_PORT}/")
+    print(f"Visit app on network: http://your-device-ip:{BACKEND_PORT}/")
+    print("Note: Closing the UI window doesn't stop the application.")
+    print("Note: Press \"CTRL + C\" to terminate the app.")
+    # Open the web browser
+    
+    web_window.show("./templates/webuiIndex.html")
+    threading.Timer(0.5, lambda: webui.wait()).start()
+    
+    # Serve the app
+    serve(app, host=OPEN_HOST, port=BACKEND_PORT)
