a14436a7dce8812d166e521db5e9d87b1a6fe491
mahi028 <tewarimohit028@gmail.com>
2025-09-24 23:22:14 +0530

    dynamic host port config

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 """This application is just a web base file explorer"""
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 from api import api
 from flask_cors import CORS
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+HOST = get_local_ip()
+OPEN_HOST = "0.0.0.0"
+PORT = 9876
 
 # Changing how jinja2 interprets delimiters
 CUSTOM_JINJA_DELIMS = {
@@ -21,20 +35,17 @@ for k, v in CUSTOM_JINJA_DELIMS.items():
 CORS(app, allow_origins=["*"])
 api.init_app(app)
 
-
-
-# Serve the frontend
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", host = HOST, port = PORT, webUI = False)
 
 @app.route("/player")
 def media_player():
-    return render_template("mediaPlayer.html")
+    return render_template("mediaPlayer.html", host = HOST, port = PORT, webUI = False)
 
 @app.route("/health")
 def health():
     return {"msg":"Backend Running"}
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=9876)
+    app.run(debug=True, host=OPEN_HOST, port=PORT)
